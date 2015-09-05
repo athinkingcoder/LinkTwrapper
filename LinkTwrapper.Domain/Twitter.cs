43d16c651e01956f1ed40b94e222163f99705727
dtndev <ianjdixon@yahoo.co.uk>
2015-09-06 00:12:21 +0100

    Use single HttpClient instance for a single Twitter instance

--- LinkTwrapper.Domain/Twitter.cs
+++ LinkTwrapper.Domain/Twitter.cs
@@ -1,17 +1,34 @@
 ﻿namespace LinkTwrapper.Domain
 {
+    using System;
     using System.Collections.Generic;
     using System.Net.Http;
     using Newtonsoft.Json;
 
     public class Twitter
     {
-        public IBearerToken RequestBearerToken(BearerTokenCredential credential)
+        private readonly HttpClient httpClient;
+
+        public Twitter()
+        {
+            this.httpClient = CreateHttpClient();
+        }
+
+        private HttpClient CreateHttpClient()
         {
             HttpClient client = new HttpClient();
-            string authorizationHeaderValue = string.Format("Basic {0}", credential.EncodedValue);
             client.DefaultRequestHeaders.Clear();
-            client.DefaultRequestHeaders.Add("Authorization", authorizationHeaderValue);
+
+            return client;
+        }
+
+        public IBearerToken RequestBearerToken(BearerTokenCredential credential)
+        {
+            string authorizationHeaderValue = string.Format("Basic {0}", credential.EncodedValue);
+            string tokenUri = "https://api.twitter.com/oauth2/token";
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, tokenUri);
+            request.Headers.Add("Authorization", authorizationHeaderValue);
 
             HttpContent payload = new FormUrlEncodedContent(new[]
             {
@@ -20,9 +37,10 @@
             payload.Headers.Clear();
             payload.Headers.Add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
 
+            request.Content = payload;
+
             BearerTokenRepsonse tokenResponse = null;
-            string tokenUri = "https://api.twitter.com/oauth2/token";
-            var responseTask = client.PostAsync(tokenUri, payload).ContinueWith(
+            var responseTask = this.httpClient.SendAsync(request).ContinueWith(
                 (completedTask) =>
                 {
                     var response = completedTask.Result;
@@ -38,15 +56,14 @@
 
         public List<Tweet> GetTweets(IBearerToken bearerToken, string screenName)
         {
-            HttpClient client = new HttpClient();
             string authorizationHeaderValue = string.Format("Bearer {0}", bearerToken.Value);
-            client.DefaultRequestHeaders.Clear();
-            client.DefaultRequestHeaders.Add("Authorization", authorizationHeaderValue);
-
-            //string response = null;
             string tweetsUri = string.Format("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={0}&count=20", screenName);
 
-            var response = client.GetAsync(tweetsUri).Result;
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, tweetsUri);
+            request.Headers.Add("Authorization", authorizationHeaderValue);
+            
+
+            var response = this.httpClient.SendAsync(request).Result;
             var json = response.Content.ReadAsStringAsync().Result;
             var tweets = JsonConvert.DeserializeObject<List<Tweet>>(json);
 
